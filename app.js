21306f3f909fa2be5f7470c18ae3371a716e62e4
F100P <tim.j.myllyla@gmail.com>
2022-06-07 23:56:14 +0200

    movement detection added, you are now able to interact with areas of the screen

--- app.js
+++ app.js
@@ -2,41 +2,59 @@ let video = document.querySelector("#video");
 let canvas = document.querySelector("#canvas");
 let context = canvas.getContext("2d");
 let main = document.getElementById("maindiv");
+let buttonCanvas = document.querySelector("#buttonCanvas");
+let buttonContext = buttonCanvas.getContext("2d");
 
-  
-
-// A bit of useful guides
-//http://smokingscript.com/pages/webcam-manipulation-with-canvas/
+let prevCanvas = document.querySelector("#prevCanvas");
+let prevContext = prevCanvas.getContext("2d");
 
 
 
 //Draws image every tick
-// This handles the display of the manipulated camera image 
+// This handles the display of the manipulated camera image
 main.addEventListener("ticks", function () {
-    //window showing videostream that we can manipulate
-    context.drawImage(video, 0, 0, 640, 480);
-  
-    //variable for manipulateable imagedata
-    let imageData = context.getImageData(0, 0, 640, 480);
-  
-    //Examle of how imagemanipulation can we performed
-    //imageData = invertedColors(imageData);
-    imageData = grayScale(imageData);
-    
-    let grid = makeGridOfImage(imageData);
-    grid = prewittOperator(grid);
-    grid = threshold(grid,64);
-    
 
+  let prevImgData =  prevContext.getImageData(0, 0, 640, 480);
+  let prevGrid = makeGridOfImage(prevImgData);
+  //prevContext.putImageData(prevImgData, 0, 0);
+  
+  context.drawImage(video, 0, 0, 640, 480);
+  //variable for manipulateable imagedata
+  let imageData = context.getImageData(0, 0, 640, 480);
 
-    imageData = gridToImage(imageData, grid);
+  //Examle of how imagemanipulation can we performed
+  //imageData = invertedColors(imageData);
+  imageData = grayScale(imageData);
   
-    //***************************** */
+
+  let grid = makeGridOfImage(imageData);
+  grid = prewittOperator(grid);
+  grid = binarize(grid,40);
+
+  prevContext.putImageData(gridToImage(imageData,grid), 0, 0);
+  //grid = removeLonelyPixels(grid);
+  grid = removeStationary(grid,prevGrid);
+  grid = openingOperation(grid);
+  imageData = gridToImage(imageData, grid);
+
+  //check if interact with blue test box
+  if(checkInteraktion(grid,0,0,150,100,100)){
+    console.log("YOU FUCKING MADE IT DUDE");
+  }
   
-    context.putImageData(imageData, 0, 0);
-  });
+  //***************************** */
+  
+  context.putImageData(imageData, 0, 0);
+  
+});
 //Create new clock and add it to the main div
 var clock = new Clock(main, 10);
+
+//Initialize blue button to indicate test touch area...
+buttonContext.beginPath();
+buttonContext.fillStyle = "blue";
+buttonContext.rect(0,0,150,100); //map a funtion to this area
+buttonContext.fill();
 
 //initiates the webcamera
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -54,8 +72,59 @@ document.getElementById("start").addEventListener("click", () => {
 
 //append clock stop to the stop button
 document.getElementById("stop").addEventListener("click", () => {
+  
   clock.stop();
 });
 
+function displayPerwitt() {
+  context.drawImage(video, 0, 0, 640, 480);
 
+  //variable for manipulateable imagedata
+  let imageData = context.getImageData(0, 0, 640, 480);
 
+  //Examle of how imagemanipulation can we performed
+  //imageData = invertedColors(imageData);
+  imageData = grayScale(imageData);
+
+  let grid = makeGridOfImage(imageData);
+  grid = prewittOperator(grid);
+
+  imageData = gridToImage(imageData, grid);
+
+  //***************************** */
+
+  context.putImageData(imageData, 0, 0);
+}
+
+function movementDetection(){
+  let prevImgData =  prevContext.getImageData(0, 0, 640, 480);
+  let prevGrid = makeGridOfImage(prevImgData);
+  //prevContext.putImageData(prevImgData, 0, 0);
+  
+  context.drawImage(video, 0, 0, 640, 480);
+  //variable for manipulateable imagedata
+  let imageData = context.getImageData(0, 0, 640, 480);
+
+  //first grayscale
+  imageData = grayScale(imageData);
+  
+
+  let grid = makeGridOfImage(imageData);
+  //prewitt to edgedetect
+  grid = prewittOperator(grid);
+  //Binary since better safe than sorry
+  grid = binarize(grid,40);
+  
+  //Save the original for next previous
+  prevContext.putImageData(gridToImage(imageData,grid), 0, 0);
+  
+  //smooth out the img
+  grid = removeStationary(grid,prevGrid);
+  grid = openingOperation(grid);
+  imageData = gridToImage(imageData, grid);
+  
+  //***************************** */
+  
+  context.putImageData(imageData, 0, 0);
+  
+}
--- fucntions.js
+++ fucntions.js
@@ -33,7 +33,7 @@ function gridToImage(imageData, pixelGrid) {
       imageData.data[x + y + 2] = pixelGrid[j][i]; //blue
     }
   }
-  
+
   return imageData;
 }
 
@@ -42,13 +42,12 @@ function makeGridOfImage(imageData) {
   //assumin we know the image is 640x480
   const img = imageData.data;
   let output = [];
-    //test variable
-    let min = 0;
+  //test variable
+  let min = 0;
   let currentRow = [];
   for (let j = 0; j < 480 * 4; j += 4) {
     for (let i = 0; i < 640 * 4; i += 4) {
       currentRow.push(img[i + 640 * j]);
-     
     }
     output.push(currentRow);
     currentRow = [];
@@ -105,7 +104,7 @@ function highPassFilter(pixelGrid) {
     [-1, 9, -1],
     [-1, -1, -1],
   ];
- 
+
   //iterate image skips edges since no zero-padding
   for (let y = 1; y < 479; y++) {
     for (let x = 1; x < 639; x++) {
@@ -126,21 +125,24 @@ function highPassFilter(pixelGrid) {
   }
   return pixelGrid;
 }
-function findSmallestValue(pixelGrid){
-    let max = 0;
-    let min  = 0;
-    for(let y = 0; y<480;y++){
-        for(let x=0 ; x<640;x++){
-            if(pixelGrid[y][x] > max){max = pixelGrid[y][x]}
-            if(pixelGrid[y][x] < min){min = pixelGrid[y][x]}
-            
-        }
+function findSmallestValue(pixelGrid) {
+  let max = 0;
+  let min = 0;
+  for (let y = 0; y < 480; y++) {
+    for (let x = 0; x < 640; x++) {
+      if (pixelGrid[y][x] > max) {
+        max = pixelGrid[y][x];
+      }
+      if (pixelGrid[y][x] < min) {
+        min = pixelGrid[y][x];
+      }
     }
-    console.log("Största värdet :"+max + "  minsta värdet:" +min)
+  }
+  console.log("Största värdet :" + max + "  minsta värdet:" + min);
 }
 //Edge detection
 function prewittOperator(pixelGrid) {
-   // 
+  //
   const xAxis = [
     [-1, 0, 1],
     [-2, 0, 2],
@@ -152,10 +154,12 @@ function prewittOperator(pixelGrid) {
     [1, 2, 1],
   ];
 
-  
-  let pixelGridY = pixelGrid.map(function(arr){return arr.slice();});
-  let pixelGridX = pixelGrid.map(function(arr){return arr.slice();});
-  
+  let pixelGridY = pixelGrid.map(function (arr) {
+    return arr.slice();
+  });
+  let pixelGridX = pixelGrid.map(function (arr) {
+    return arr.slice();
+  });
 
   for (let y = 1; y < 479; y++) {
     for (let x = 1; x < 639; x++) {
@@ -166,40 +170,49 @@ function prewittOperator(pixelGrid) {
         for (let kx = -1; kx <= 1; kx++) {
           //Do the perwitt x and y on the same iteration
           let posY = y + ky;
-          
+
           let posX = x + kx;
-          
-         
-          //Något här fungerar inte som det ska... 
+
+          //Något här fungerar inte som det ska...
           let val = pixelGrid[posY][posX];
-          
+
           //if(posX == 1 && posY ==1){console.log(pixelGrid[1][1] +" and "+pixelGrid[posY][posX] + " at coordinate " + posY +" : " + posX + "  kx=" + kx + "  ky=" +ky + " x n y ="+ x + " " +y)}
-          
-        
-          sumY += (val* yAxis[ky + 1][kx + 1]);
-          sumX += (val* xAxis[ky + 1][kx + 1]);
+
+          sumY += val * yAxis[ky + 1][kx + 1];
+          sumX += val * xAxis[ky + 1][kx + 1];
         }
       }
-      
-      
+
       pixelGridY[y][x] = sumY;
       pixelGridX[y][x] = sumX;
     }
   }
-  
+
   //might do this in its own function, but combine the value of perwitt x and y
-  for(let y = 0; y<480;y++){
-      for(let x=0 ; x<640;x++){
-          pixelGrid[y][x] =  (Math.sqrt(Math.pow(pixelGridY[y][x],2)+Math.pow(pixelGridX[y][x],2)));
-          
-      }
+  for (let y = 0; y < 480; y++) {
+    for (let x = 0; x < 640; x++) {
+      pixelGrid[y][x] = Math.sqrt(
+        Math.pow(pixelGridY[y][x], 2) + Math.pow(pixelGridX[y][x], 2)
+      );
+    }
   }
 
   return pixelGrid;
-
 }
 
+//sets all pixels in given pixelgrid that has a lower value than threshold to 0
 function threshold(pixelGrid, thresValue) {
+  for (let y = 0; y < 480; y++) {
+    for (let x = 0; x < 640; x++) {
+      if (pixelGrid[y][x] < thresValue) {
+        pixelGrid[y][x] = 0;
+      }
+    }
+  }
+  return pixelGrid;
+}
+//Binarize given pixelgrid using the threshold. result grid has pixelvaules 0 or 255
+function binarize(pixelGrid, thresValue) {
   for (let y = 0; y < 480; y++) {
     for (let x = 0; x < 640; x++) {
       if (pixelGrid[y][x] < thresValue) {
@@ -208,4 +221,141 @@ function threshold(pixelGrid, thresValue) {
     }
   }
   return pixelGrid;
+}
+
+//Removes small and lonely pixel clusters. max 4px big. recommended to use binarized grid.
+function removeLonelyPixels(pixelGrid) {
+  for (let y = 1; y < 479; y++) {
+    for (let x = 1; x < 639; x++) {
+      //iterate kernel
+      let sum = 0; //kernel sum of pixel
+      for (let ky = -1; ky <= 1; ky++) {
+        for (let kx = -1; kx <= 1; kx++) {
+          //Somehow it interperet -1 as a string so a typical consol.log() results in "64 -1".... Stupid
+          let posY = y + ky;
+          let posX = x + kx;
+          let val = pixelGrid[posY][posX];
+          if (val > 0) {
+            sum++;
+          }
+        }
+      }
+      if (sum <= 4) {
+        pixelGrid[y][x] = 0;
+      }
+    }
+  }
+  return pixelGrid;
+}
+
+//Problemet är att den glömmer bort vad den har tagit bort så när det kommer ny information så glömmer den bort att ta bort det XD
+//prev måste vara en obehandlad version av new
+function removeStationary(grid, prevGrid) {
+  tempGrid = prevGrid.map(function (arr) {
+    return arr.slice();
+  });
+
+  if (prevGrid[0][0]) {
+    for (let y = 0; y < 480; y++) {
+      for (let x = 0; x < 640; x++) {
+        grid[y][x] -= tempGrid[y][x];
+
+        //grid[y][x] -= prevGrid[y][x]*0.5
+      }
+    }
+  }
+  return grid;
+}
+
+function erosionOperation(pixelGrid) {
+  let structElement = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+  ];
+
+  tempGrid = pixelGrid.map(function (arr) {
+    return arr.slice();
+  });
+
+  for (let y = 1; y < 479; y++) {
+    for (let x = 1; x < 639; x++) {
+      //iterate kernel
+      let sum = 0; //kernel sum of pixel
+      for (let ky = -1; ky <= 1; ky++) {
+        for (let kx = -1; kx <= 1; kx++) {
+          //Somehow it interperet -1 as a string so a typical consol.log() results in "64 -1".... Stupid
+          let posY = y + ky;
+          let posX = x + kx;
+          let val = pixelGrid[posY][posX];
+          if (val > 0) {
+            sum++;
+          }
+        }
+      }
+      if (sum != 9) {
+        tempGrid[y][x] = 0;
+      } else {
+        tempGrid[y][x] = 255;
+      }
+    }
+  }
+  return tempGrid;
+}
+
+function dilationOperation(pixelGrid) {
+  tempGrid = pixelGrid.map(function (arr) {
+    return arr.slice();
+  });
+
+  for (let y = 1; y < 479; y++) {
+    for (let x = 1; x < 639; x++) {
+      //iterate kernel
+      let sum = 0; //kernel sum of pixel
+      for (let ky = -1; ky <= 1; ky++) {
+        for (let kx = -1; kx <= 1; kx++) {
+          //Somehow it interperet -1 as a string so a typical consol.log() results in "64 -1".... Stupid
+          let posY = y + ky;
+          let posX = x + kx;
+          let val = pixelGrid[posY][posX];
+          if (val > 0) {
+            sum++;
+          }
+        }
+      }
+      if (sum > 0) {
+        tempGrid[y][x] = 255;
+      } else {
+        tempGrid[y][x] = 0;
+      }
+    }
+  }
+  return tempGrid;
+}
+
+function closingOperation(pixelGrid) {
+  return erosionOperation(dilationOperation(pixelGrid));
+}
+
+function openingOperation(pixelGrid) {
+  return dilationOperation(erosionOperation(pixelGrid));
+}
+
+//checks a area for interaction returns true if numbers of active pixels exceeds threshold
+function checkInteraktion(pixelGrid, TL, BL, TR, BR, thresHold) {
+  let sum = 0;
+  for (let y = TL; y < BR; y++) {
+    for (let x = BL; x < TR; x++) {
+      if (pixelGrid[y][x] > 0) {
+        sum++;
+      }
+    }
+ 
+  }
+  
+  if (sum >= thresHold) {
+    return true;
+  } else {
+    return false;
+  }
 }
